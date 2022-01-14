7d0963b4f1d2e0c8f646bf007d421457cfa800b5
xyntechx <xyntechx@gmail.com>
2022-01-14 08:00:19 +0800

    🐛 getStaticProps bugfix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,50 @@
-import type { NextPage } from "next";
+import type { NextPage, GetStaticProps, InferGetStaticPropsType } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useState } from "react";
-
 import { gameEvents } from "../public/json/gameEvents";
 
-const Home: NextPage = ({ slides }) => {
-
-    const[slide, setSlide] = useState<JSX.Element>( // no idea if i should put jsx into a state or if there's something better
+const Home: NextPage = ({
+    slides,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+    const [slide, setSlide] = useState(
         <div>
             <button onClick={() => showSlide(0)}>Start Game</button>
         </div>
-    )
+    );
 
     const showSlide = async (index: number) => {
         if (slides[index].payday != null) {
-            setSlide(
-                <p>Payday</p>
-            )
+            setSlide(<p>Payday</p>);
         } else {
             setSlide(
                 <div>
-                    <div className={styles.text}>{ slides[index].text }</div>
+                    <div className={styles.text}>{slides[index].text}</div>
                     <div>
-                        <button onClick={() => results(index, 0)}>{ slides[index].options[0].option }</button>
-                        <button onClick={() => results(index, 1)}>{ slides[index].options[1].option }</button>
+                        <button onClick={() => results(index, 0)}>
+                            {slides[index].options[0].option}
+                        </button>
+                        <button onClick={() => results(index, 1)}>
+                            {slides[index].options[1].option}
+                        </button>
                     </div>
                 </div>
-            )
+            );
         }
-    }
+    };
 
     const results = async (index: number, option: number) => {
         // 0: first option, 1: second option
         setSlide(
             <div>
-                <div className={styles.text}>{ slides[index].options[option].text }</div>
+                <div className={styles.text}>
+                    {slides[index].options[option].text}
+                </div>
                 <button onClick={() => nextSlide(index)}>Next</button>
             </div>
-        )
-    }
+        );
+    };
 
     const nextSlide = async (index: number) => {
         if (index + 1 > slides.length) {
@@ -48,11 +52,11 @@ const Home: NextPage = ({ slides }) => {
                 <div>
                     <div className={styles.text}>Game Finished</div>
                 </div>
-            )
+            );
         } else {
-            showSlide(index + 1)
+            showSlide(index + 1);
         }
-    }
+    };
 
     return (
         <div className={styles.container}>
@@ -67,18 +71,18 @@ const Home: NextPage = ({ slides }) => {
                     Raising awareness about the challenges faced by the elderly
                     in Singapore
                 </h1>
-                { slide }
+                {slide}
             </main>
         </div>
     );
 };
 
-export const getStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async (context) => {
     return {
         props: {
-            slides: gameEvents
-        }
-    }
-}
+            slides: gameEvents,
+        },
+    };
+};
 
 export default Home;
