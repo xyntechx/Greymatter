fc9c6d45a1e7430cfc1e124ca8d3e13665b26852
xyntechx <xyntechx@gmail.com>
2022-01-31 00:06:02 +0800

    ✨ Remove slides from home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,63 +1,8 @@
-import type { NextPage, GetStaticProps, InferGetStaticPropsType } from "next";
+import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import { useState } from "react";
-import { gameEvents } from "../public/json/gameEvents";
 
-const Home: NextPage = ({
-    slides,
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
-    const [slide, setSlide] = useState(
-        <div>
-            <button onClick={() => showSlide(0)}>Start Game</button>
-        </div>
-    );
-
-    const showSlide = async (index: number) => {
-        if (slides[index].payday != null) {
-            setSlide(<p>Payday</p>);
-        } else {
-            setSlide(
-                <div>
-                    <div className={styles.text}>{slides[index].text}</div>
-                    <div>
-                        <button onClick={() => results(index, 0)}>
-                            {slides[index].options[0].option}
-                        </button>
-                        <button onClick={() => results(index, 1)}>
-                            {slides[index].options[1].option}
-                        </button>
-                    </div>
-                </div>
-            );
-        }
-    };
-
-    const results = async (index: number, option: number) => {
-        // 0: first option, 1: second option
-        setSlide(
-            <div>
-                <div className={styles.text}>
-                    {slides[index].options[option].text}
-                </div>
-                <button onClick={() => nextSlide(index)}>Next</button>
-            </div>
-        );
-    };
-
-    const nextSlide = async (index: number) => {
-        if (index + 1 > slides.length) {
-            setSlide(
-                <div>
-                    <div className={styles.text}>Game Finished</div>
-                </div>
-            );
-        } else {
-            showSlide(index + 1);
-        }
-    };
-
+const Home: NextPage = () => {
     return (
         <div className={styles.container}>
             <Head>
@@ -71,18 +16,9 @@ const Home: NextPage = ({
                     Raising awareness about the challenges faced by the elderly
                     in Singapore
                 </h1>
-                {slide}
             </main>
         </div>
     );
-};
-
-export const getStaticProps: GetStaticProps = async (context) => {
-    return {
-        props: {
-            slides: gameEvents,
-        },
-    };
 };
 
 export default Home;
